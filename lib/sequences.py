e81b0b5bb49de20ba1e1f94f257c5a38b6502a7e
deborah <fabbydebby@gmail.com>
2024-05-22 15:45:04 +0300

    python-3-sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    
+    fibonacci_sequence = []
+    
+    for i in range(length):
+        if i == 0:
+            fibonacci_sequence.append(0)
+        elif i == 1:
+            fibonacci_sequence.append(1)
+        else:
+            fibonacci_sequence.append(fibonacci_sequence[-1] + fibonacci_sequence[-2])
+    
+    print(fibonacci_sequence)
